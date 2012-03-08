ab39c66a9d473fbd81627b26e5cab426b8e967a8
Marius <mariush@lr.co.za>
2012-03-08 16:55:38 +0200

    First commit

---
+++ testGit/src/za/co/lr/test/test1.java
@@ -0,0 +1,19 @@
+/*
+ * Created on 2012/03/08
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package za.co.lr.test;
+
+/**
+ * @author Mariush
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class test1 {
+
+	public static void main(String[] args) {
+	}
+}
